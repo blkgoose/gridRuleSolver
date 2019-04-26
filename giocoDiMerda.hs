a34711631fdd5b5e905551383f2fcdf9d81d99fa
blkgoose <alebian1996@gmail.com>
2019-04-26 18:33:44 +0200

    improvement: Finished tree

--- giocoDiMerda.hs
+++ giocoDiMerda.hs
@@ -1,22 +1,57 @@
 module Main where
 
+import Data.List
+import Data.Ord
+
 data Tree a = Node a [Tree a] deriving Show
+type Node = Tree [Vec]
 
-main = print out
+type Vec = (Int, Int)
+
+(<+>) (ax,ay) (bx, by) = (ax+bx, ay+by)
+(|>) x f = f x
+
+nodeVal :: Tree a -> a
+nodeVal (Node v _) = v
+
+nodeBra :: Tree a -> [Tree a]
+nodeBra (Node _ b) = b
+
+inList :: (Foldable t, Eq a) => t a -> a -> Bool
+inList = flip elem
+
+main = print solution
     where
-        start = Node 1 []
-        steps = 5
-        moves = [-3, 3, -30, 30, -22, 22, -18, 18]
-
-        next :: Int -> [Int]
-        next p = map (+p) moves
-        -- Add bounds
-
-        gen :: Tree Int -> Int -> Tree Int
-        gen p 0 = p
-        gen (Node v bra) n = Node v nb
+        next :: Node -> [Node]
+        next (Node pos _) = out
             where
-                nb = map (\a -> Node a []) x
-                x  = filter (>0) $ filter (<=100) $ next v
+            at    = last pos
+            moves = [(0,3),(0,-3),(3,0),(-3,0),(2,2),(-2,2),(2,-2),(-2,-2)]
+            out = moves
+                  |> map    (at <+>)
+                  |> filter (\p -> not $ elem p pos)
+                  |> filter ((>0)   . fst)
+                  |> filter ((<=10) . fst)
+                  |> filter ((>0)   . snd)
+                  |> filter ((<=10) . snd)
+                  |> map    (\x -> Node (pos ++ [x]) [])
 
-        out = gen start 1
+        gen :: Int -> [Node] -> [Node]
+        gen 0 nodes = nodes
+        gen n nodes = map (\oldNode -> Node (nodeVal oldNode) (gen (n-1) $ next oldNode)) nodes
+
+        steps = 50
+
+        endNodes :: Node -> [[Vec]]
+        endNodes (Node v []) = [v]
+        endNodes (Node _ b) = concat $ map endNodes b
+
+        tree :: [Node]
+        tree = gen steps [(Node [(1,1)] [])]
+
+        solution = head tree
+                   |> endNodes
+                   |> sortBy (comparing longer)
+                   |> take 1
+            where
+                longer = negate . length
